48e32e23c7a0709f1568c46a1d3f7f048586a9cf
Arbie Samong <phektus@gmail.com>
2014-02-18 21:40:57 +0800

    used fades for transition

--- js/sliderize.js
+++ js/sliderize.js
@@ -1,22 +1,16 @@
-$.fn.sliderize = function() {
+$.fn.sliderize = function(params) {
     var that = this;
     var ctr = 0;
-    var children = that.children();   
-    var clear = function() {
-        $.map(children, function(child) {
-            $(child).css('list-style', 'none');
-            $(child).hide();
-        });
-    };
+    var children = that.children();
+    var slideSpeed = params.slideSpeed || 100;
 
-    
-    clear();
-    $(children[ctr]).show();
+    children.css('list-style', 'none');
+    children.not(':first').hide();
     setInterval(function() {
-        clear();
-        if(ctr>0) $(children[ctr-1]).hide();
-        $(children[ctr]).show();
-        ctr++;
-        if(ctr === children.length-1) ctr = 0;
-    }, 2000);
+        children.eq(ctr).fadeOut(slideSpeed, function() {
+            ctr++;
+            if(ctr === children.length-1) ctr = 0;
+            children.eq(ctr).fadeIn(slideSpeed);
+        });
+    }, 2 * 1000);
 };
