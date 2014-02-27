e6221e931e8baa2b4ebd4c7d9799a0d4b72821a8
Arbie Samong <phektus@gmail.com>
2014-02-27 20:53:03 +0800

    changed from fade to slid

--- jquery.sliderize.js
+++ jquery.sliderize.js
@@ -7,6 +7,7 @@
             var children = $(that).children();
             var params = _params || {};
             var slideSpeed = params.slideSpeed || 100;
+            var slideWidth = params.maxWidth + (params.padding|0);
             var _id = $(that).attr('id');
             var justSlid = false;
             var slide;
@@ -17,6 +18,7 @@
                 $(that).attr('id', _id);
             }
             $(that).addClass('slider-list');
+            $(that).addClass('slider-list-'+_id);
 
             // create wrapper and append this element
             $(that).parent().append([
@@ -26,21 +28,38 @@
             ].join(''));
             $(that).wrap($('#slider-wrapper-'+_id));
 
+            $('#slider-wrapper-'+_id).css('width', params.maxWidth + 'px');
+            $('.slider-list-'+_id).css('width', params.maxWidth + 'px');
+            $('.slider-list-'+_id+' li').css('width', params.maxWidth + 'px');
+
             // remove bullet and hide all
             children.css('list-style', 'none');
             children.not(':first').hide();
 
             // slider counter
-            slide = function(advance) {
-                // fade out current         
-                children.eq(ctr).fadeOut(slideSpeed, function() {
-                    ctr += advance ? 1 : -1;
-                    if(advance && ctr === children.length) ctr = 0;
-                    else if(!advance && ctr === 0) ctr = children.length;
-                    // show next
-                    children.eq(ctr).fadeIn(slideSpeed);
+            slide = function(currentChild, nextChild) {
+                currentChild.css('display', 'block')
+                    .css('top', 0)
+                    .css('left', 0)
+                    .css('position', 'absolute');                    
+                console.log('current child:', $(currentChild).css('margin-left'));
+
+                currentChild.animate({
+                    marginLeft: '-=' + slideWidth + 'px'
+                });
+
+                nextChild.css('margin-left', slideWidth+'px')
+                    .css('display', 'block')
+                    .css('top', 0)
+                    .css('left', 0)
+                    .css('position', 'relative');
+                console.log('next child:', $(nextChild).css('margin-left'));
+
+                nextChild.animate({
+                    marginLeft: '0'
                 });
             };
+
             setInterval(function() {
                 // skip slide if controls were just pressed
                 if(justSlid === true) {
@@ -48,8 +67,18 @@
                     return;
                 }
                 // perform slide
-                slide.call(that, true);
-            }, params.maxWait || 2000);
+
+                var currentChild;
+                var nextChild;
+                var advance = true;
+                currentChild = children.eq(ctr);
+
+                ctr++;
+                if(ctr >= children.length) ctr = 0;
+                nextChild = children.eq(ctr);
+
+                slide.call(that, currentChild, nextChild);
+            }, params.maxWait || 1000);
 
             // create controls
             $('#slider-wrapper-'+_id).append('<a href="#" id="btn-prev-'+_id+'"><</a>');
